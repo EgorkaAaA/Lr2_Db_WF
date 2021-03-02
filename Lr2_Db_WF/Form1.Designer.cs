76931a00ada28f4b139a67278e8d42d306ef5254
Егор Лебедев <m123ry132@gmail.com>
2021-03-02 16:39:38 +0700

    Але

--- Lr2_Db_WF/Form1.Designer.cs
+++ Lr2_Db_WF/Form1.Designer.cs
@@ -28,55 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.bakeryDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.bakeryDataSet = new Lr2_Db_WF.BakeryDataSet();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.bakeryDataSetBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.bakeryDataSet)).BeginInit();
+            this.Grid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // Grid
             // 
-            this.dataGridView1.AutoGenerateColumns = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.DataSource = this.bakeryDataSetBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 3);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(539, 323);
-            this.dataGridView1.TabIndex = 0;
+            this.Grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grid.Location = new System.Drawing.Point(0, 0);
+            this.Grid.Name = "Grid";
+            this.Grid.Size = new System.Drawing.Size(612, 323);
+            this.Grid.TabIndex = 0;
             // 
-            // bakeryDataSetBindingSource
+            // label1
             // 
-            this.bakeryDataSetBindingSource.DataSource = this.bakeryDataSet;
-            this.bakeryDataSetBindingSource.Position = 0;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(644, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
             // 
-            // bakeryDataSet
+            // label2
             // 
-            this.bakeryDataSet.DataSetName = "BakeryDataSet";
-            this.bakeryDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(644, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(644, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(712, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(712, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(712, 106);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Булочная",
+            "Сотрудники",
+            "Смены",
+            "Поставки "});
+            this.comboBox1.Location = new System.Drawing.Point(618, 0);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.MouseLeave += new System.EventHandler(this.comboBox1_MouseLeave_1);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(618, 306);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(125, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "режим изменнений";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(737, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Добавить ";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(905, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Grid);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.bakeryDataSetBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.bakeryDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.BindingSource bakeryDataSetBindingSource;
-        private BakeryDataSet bakeryDataSet;
+        private System.Windows.Forms.DataGridView Grid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Lr2_Db_WF/Form1.cs
+++ Lr2_Db_WF/Form1.cs
@@ -15,7 +15,484 @@ namespace Lr2_Db_WF
         public Form1()
         {
             InitializeComponent();
+            Invisible();
         }
 
+        private void Invisible()
+        {
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            textBox3.Visible = false;
+        }
+
+        private void BackerySetDataGridFill()
+        {
+            BakeryEntities db = new BakeryEntities();
+            int b = 0;
+            foreach (var a in db.BackerySet)
+            {
+                b++;
+            }
+            int i = 1;
+            Grid.ColumnCount = 2;
+            Grid.RowCount = b + 1;
+            Grid.Rows[0].Cells[1].Value = "Адрес";
+            Grid.ColumnHeadersVisible = false;
+
+
+            foreach (var k in db.BackerySet)
+            {
+                Grid.Rows[i].Cells[1].Value = k.adres;
+                i++;
+            }
+            DeleteButtom(Grid.RowCount);
+        }
+
+        private void manufactureDataGridFill()
+        {
+            BakeryEntities db = new BakeryEntities();
+            int b = 0;
+            foreach (var a in db.StaffSet)
+            {
+                b++;
+            }
+            int i = 1;
+            Grid.ColumnCount = 4;
+            Grid.RowCount = b + 1;
+            Grid.Rows[0].Cells[1].Value = "Фамилия";
+            Grid.Rows[0].Cells[2].Value = "Имя";
+            Grid.Rows[0].Cells[3].Value = "Отчество";
+            Grid.ColumnHeadersVisible = false;
+
+
+            foreach (var m in db.StaffSet)
+            {
+                Grid.Rows[i].Cells[1].Value = m.Surname;
+                Grid.Rows[i].Cells[2].Value = m.name;
+                Grid.Rows[i].Cells[3].Value = m.patronymic;
+                i++;
+            }
+            DeleteButtom(Grid.RowCount);
+        }
+
+        private void ShiftsSetDataGridFill()
+        {
+            BakeryEntities db = new BakeryEntities();
+            int b = 0;
+            foreach (var a in db.ShiftsSet)
+            {
+                b++;
+            }
+            int i = 1;
+            Grid.ColumnCount = 4;
+            Grid.RowCount = b + 1;
+            Grid.Rows[0].Cells[1].Value = "Выполнен ли план";
+            Grid.Rows[0].Cells[2].Value = "Сотрудник";
+            Grid.Rows[0].Cells[3].Value = "Булочная";
+            Grid.ColumnHeadersVisible = false;
+
+
+            foreach (var f in db.ShiftsSet)
+            {
+                Grid.Rows[i].Cells[1].Value = f.Plan;
+                Grid.Rows[i].Cells[2].Value = f.Staff_Id;
+                Grid.Rows[i].Cells[3].Value = f.Backery_Id; ;
+                i++;
+            }
+            DeleteButtom(Grid.RowCount);
+        }
+
+        private void DeliveriesSetDataGridFill()
+        {
+            BakeryEntities db = new BakeryEntities();
+            int b = 0;
+            foreach (var a in db.DeliveriesSet)
+            {
+                b++;
+            }
+            int i = 1;
+            Grid.ColumnCount = 4;
+            Grid.RowCount = b + 1;
+            Grid.Rows[0].Cells[1].Value = "Адресс доставки";
+            Grid.Rows[0].Cells[2].Value = "Булочная";
+            Grid.Rows[0].Cells[3].Value = "Количестов тортов";
+            Grid.ColumnHeadersVisible = false;
+
+
+            foreach (var d in db.DeliveriesSet)
+            {
+                Grid.Rows[i].Cells[1].Value = d.adres;
+                Grid.Rows[i].Cells[2].Value = d.Backery_Id;
+                Grid.Rows[i].Cells[3].Value = d.numberOfCakes;
+                i++;
+            }
+            DeleteButtom(Grid.RowCount);
+        }
+
+        private void DeleteButtom(int rowsCount)
+        {
+            for (int i = 1; i < rowsCount; i++)
+            {
+                DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+                Grid[0, i] = linkCell;
+                Grid[0, i].Value = "Удалить";
+            }
+        }
+
+        private void comboBox1_MouseLeave(object sender, EventArgs e)
+        {
+            if (!checkBox1.Checked)
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    Invisible();
+                    BackerySetDataGridFill();
+                    label1.Visible = true;
+                    label1.Text = "Адрес: ";
+                    textBox1.Visible = true;
+                }
+                else if (comboBox1.SelectedIndex == 1)
+                {
+                    Invisible();
+                    manufactureDataGridFill();
+                    label1.Visible = true;
+                    label2.Visible = true;
+                    label3.Visible = true;
+                    label1.Text = "Фамилия";
+                    label2.Text = "Имя";
+                    label3.Text = "Отчество";
+                    textBox1.Visible = true;
+                    textBox2.Visible = true;
+                    textBox3.Visible = true;
+                }
+                else if (comboBox1.SelectedIndex == 2)
+                {
+                    Invisible();
+                    ShiftsSetDataGridFill();
+                    label1.Visible = true;
+                    label2.Visible = true;
+                    label3.Visible = true;
+                    label1.Text = "Выполнен ли план";
+                    label2.Text = "Сотрудник";
+                    label3.Text = "Булочная";
+                    textBox1.Visible = true;
+                    textBox2.Visible = true;
+                    textBox3.Visible = true;
+                }
+                else if (comboBox1.SelectedIndex == 3)
+                {
+                    Invisible();
+                    DeliveriesSetDataGridFill();
+                    label1.Visible = true;
+                    label2.Visible = true;
+                    label3.Visible = true;
+                    label1.Text = "Адресс доставки";
+                    label2.Text = "Булочная";
+                    label3.Text = "Количестов тортов";
+                    textBox1.Visible = true;
+                    textBox2.Visible = true;
+                    textBox3.Visible = true;
+                }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                BackerySet BackerySet = new BackerySet { adres = textBox1.Text };
+                BackerySet.BackerySetAdd(BackerySet);
+                BackerySetDataGridFill();
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                StaffSet StaffSet = new StaffSet
+                {
+                    Surname = textBox1.Text,
+                    name = textBox2.Text,
+                    patronymic = textBox3.Text
+                };
+                StaffSet.StaffSetAdd(StaffSet);
+                manufactureDataGridFill();
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                ShiftsSet ShiftsSet = new ShiftsSet
+                {
+                    Plan = textBox1.Text,
+                    Backery_Id = Convert.ToInt32(textBox3.Text),
+                    Staff_Id= Convert.ToInt32(textBox2.Text)
+                };
+                ShiftsSet.ShiftsSetAdd(ShiftsSet);
+                ShiftsSetDataGridFill();
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                DeliveriesSet DeliveriesSet = new DeliveriesSet
+                {
+                    adres = textBox1.Text,
+                    Backery_Id = Convert.ToInt32(textBox2.Text),
+                    numberOfCakes = Convert.ToInt16(textBox3.Text)
+                };
+                DeliveriesSet.DeliveriesSetAdd(DeliveriesSet);
+                DeliveriesSetDataGridFill();
+            }
+        }
+
+        private void Grid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            BakeryEntities db = new BakeryEntities();
+            if (e.ColumnIndex == 0)
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    int i = 0;
+                    BackerySet BackerySet = new BackerySet();
+                    foreach (var a in db.BackerySet)
+                    {
+                        if (i == e.RowIndex - 1)
+                        {
+                            BackerySet = a;
+                        }
+                        i++;
+
+                    }
+                    db.BackerySet.Remove(BackerySet);
+                    db.SaveChanges();
+                    BackerySetDataGridFill();
+                }
+                else if (comboBox1.SelectedIndex == 1)
+                {
+                    int i = 0;
+                    StaffSet StaffSet = new StaffSet();
+                    foreach (var a in db.StaffSet)
+                    {
+                        if (i == e.RowIndex - 1)
+                        {
+                            StaffSet = a;
+                        }
+                        i++;
+                    }
+                    db.StaffSet.Remove(StaffSet);
+                    db.SaveChanges();
+                    manufactureDataGridFill();
+                }
+                else if (comboBox1.SelectedIndex == 2)
+                {
+                    int i = 0;
+                    ShiftsSet ShiftsSet = new ShiftsSet();
+                    foreach (var a in db.ShiftsSet)
+                    {
+                        if (i == e.RowIndex - 1)
+                        {
+                            ShiftsSet = a;
+                        }
+                        i++;
+                    }
+                    db.ShiftsSet.Remove(ShiftsSet);
+                    db.SaveChanges();
+                    ShiftsSetDataGridFill();
+                }
+                else if (comboBox1.SelectedIndex == 3)
+                {
+                    int i = 0;
+                    DeliveriesSet DeliveriesSet = new DeliveriesSet();
+                    foreach (var a in db.DeliveriesSet)
+                    {
+                        if (i == e.RowIndex - 1)
+                        {
+                            DeliveriesSet = a;
+                        }
+                        i++;
+                    }
+                    db.DeliveriesSet.Remove(DeliveriesSet);
+                    db.SaveChanges();
+                    DeliveriesSetDataGridFill();
+                }
+            }
+        }
+
+        private void Grid_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            BakeryEntities db = new BakeryEntities();
+            if (checkBox1.Checked)
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    if (e.ColumnIndex == 1)
+                    {
+                        int i = 0;
+                        foreach (var a in db.BackerySet)
+                        {
+                            if (i == e.RowIndex - 1)
+                            {
+                                a.adres = Grid[1, e.RowIndex].Value.ToString();
+                            }
+                            i++;
+                        }
+                        db.SaveChanges();
+                        BackerySetDataGridFill();
+                    }
+
+                }
+                else if (comboBox1.SelectedIndex == 1)
+                {
+                    if (e.ColumnIndex == 1)
+                    {
+                        int i = 0;
+                        foreach (var a in db.StaffSet)
+                        {
+                            if (i == e.RowIndex - 1)
+                            {
+                                a.Surname = Grid[1, e.RowIndex].Value.ToString();
+                            }
+                            i++;
+                        }
+                        db.SaveChanges();
+                        manufactureDataGridFill();
+                    }
+                    else if (e.ColumnIndex == 2)
+                    {
+                        int i = 0;
+                        foreach (var a in db.StaffSet)
+                        {
+                            if (i == e.RowIndex - 1)
+                            {
+                                a.name = Grid[1, e.RowIndex].Value.ToString();
+                            }
+                            i++;
+                        }
+                        db.SaveChanges();
+                        manufactureDataGridFill();
+                    }
+                    else if (e.ColumnIndex == 3)
+                    {
+                        int i = 0;
+                        foreach (var a in db.StaffSet)
+                        {
+                            if (i == e.RowIndex - 1)
+                            {
+                                a.patronymic = Grid[1, e.RowIndex].Value.ToString();
+                            }
+                            i++;
+                        }
+                        db.SaveChanges();
+                        manufactureDataGridFill();
+                    }
+                }
+                else if (comboBox1.SelectedIndex == 3)
+                {
+                    if (e.ColumnIndex == 1)
+                    {
+                        int i = 0;
+                        foreach (var a in db.DeliveriesSet)
+                        {
+                            if (i == e.RowIndex - 1)
+                            {
+                                a.adres = Grid[1, e.RowIndex].Value.ToString();
+                            }
+                            i++;
+                        }
+                        db.SaveChanges();
+                        DeliveriesSetDataGridFill();
+                    }
+                    else if (e.ColumnIndex == 3)
+                    {
+                        int i = 0;
+                        foreach (var a in db.DeliveriesSet)
+                        {
+                            if (i == e.RowIndex - 1)
+                            {
+                                a.Backery_Id = Convert.ToInt32(Grid[1, e.RowIndex].Value.ToString());
+                            }
+                            i++;
+                        }
+                        db.SaveChanges();
+                        DeliveriesSetDataGridFill();
+                    }
+                    else if (e.ColumnIndex == 2)
+                    {
+                        int i = 0;
+                        foreach (var a in db.DeliveriesSet)
+                        {
+                            if (i == e.RowIndex - 1)
+                            {
+                                foreach (var b in db.BackerySet)
+                                {
+                                    if (b.Id == Convert.ToInt32(Grid[1, e.RowIndex].Value.ToString()))
+                                    {
+                                        a.Backery_Id = Convert.ToInt32(Grid[1, e.RowIndex].Value.ToString());
+                                    }
+                                }
+                            }
+                            i++;
+                        }
+                        db.SaveChanges();
+                        DeliveriesSetDataGridFill();
+                    }
+                }
+                else if (comboBox1.SelectedIndex == 2)
+                {
+                    if (e.ColumnIndex == 1)
+                    {
+                        int i = 0;
+                        foreach (var a in db.ShiftsSet)
+                        {
+                            if (i == e.RowIndex - 1)
+                            {
+                                a.Plan = Grid[1, e.RowIndex].Value.ToString();
+                            }
+                            i++;
+                        }
+                        db.SaveChanges();
+                        ShiftsSetDataGridFill();
+                    }
+                    else if (e.ColumnIndex == 2)
+                    {
+                        int i = 0;
+                        foreach (var a in db.ShiftsSet)
+                        {
+                            if (i == e.RowIndex - 1)
+                            {
+                                foreach (var b in db.BackerySet)
+                                {
+                                    if (b.Id == Convert.ToInt32(Grid[1, e.RowIndex].Value.ToString()))
+                                    {
+                                        a.Backery_Id = Convert.ToInt32(Grid[1, e.RowIndex].Value.ToString());
+                                    }
+                                }
+                            }
+                            i++;
+                        }
+                        db.SaveChanges();
+                        ShiftsSetDataGridFill();
+                    }
+                    else if (e.ColumnIndex == 3)
+                    {
+                        int i = 0;
+                        foreach (var a in db.ShiftsSet)
+                        {
+                            if (i == e.RowIndex - 1)
+                            {
+                                foreach (var b in db.StaffSet)
+                                {
+                                    if (b.Id == Convert.ToInt32(Grid[1, e.RowIndex].Value.ToString()))
+                                    {
+                                        a.Staff_Id = Convert.ToInt32(Grid[1, e.RowIndex].Value.ToString());
+                                    }
+                                }
+                            }
+                            i++;
+                        }
+                        db.SaveChanges();
+                        ShiftsSetDataGridFill();
+                    }
+                }
+        }
+
+        private void comboBox1_MouseLeave_1(object sender, EventArgs e)
+        {
+            comboBox1_MouseLeave(sender, e);
+        }
     }
 }
